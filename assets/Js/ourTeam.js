e84c123936719d47c1c5106d9c9cec1f59a44ea0
sam908qwe <mohammad8445sameer@gmail.com>
2025-04-27 18:52:31 +0530

    our team section chnaged again

--- assets/Js/ourTeam.js
+++ assets/Js/ourTeam.js
@@ -5,31 +5,18 @@ let teamBoxContainer = document.querySelector(".team-box-container");
 
 teamBoxes.forEach((box) => {
     const teamBoxOverlay = box.querySelector(".team-box-overlay");
-    const teamShareBut = box.querySelector(".team-share-but");
-    const teamSocialHandels = box.querySelector(".team-social-handels");
+
+    if(!teamBoxOverlay) return;
+
     box.addEventListener("mouseover", () => {
         teamBoxOverlay.classList.add("team-box-overlay-effect");
         teamBoxOverlay.classList.remove("team-box-overlay-outer");
-
-        teamShareBut.classList.add(".team-share-but-effect");
-        teamShareBut.classList.remove(".team-share-but-eftOut");
-
-        teamSocialHandels.style.visibility = "visible"; 
-        teamSocialHandels.classList.add(".team-social-handels-effect");
-        teamSocialHandels.classList.remove(".team-social-handels-eftOut");
-        console.log("sameer");
     })
 
     box.addEventListener("mouseleave", (evts) => {
             teamBoxOverlay.classList.remove("team-box-overlay-effect");
             teamBoxOverlay.classList.add("team-box-overlay-outer");
-
-            teamShareBut.classList.remove(".team-share-but-effect");
-            teamShareBut.classList.add(".team-share-but-eftOut");
-
-            // teamSocialHandels.style.visibility = "hidden";
-            teamSocialHandels.classList.remove(".team-social-handels-effect");
-            teamSocialHandels.classList.add(".team-social-handels-eftOut");
     } )
 })
+
 
