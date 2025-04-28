9346edba3471596fa7017d891e7ed55589ffd297
sam908qwe <mohammad8445sameer@gmail.com>
2025-04-28 16:40:04 +0530

    Apna message yahan likho

--- assets/Js/ourTeam.js
+++ assets/Js/ourTeam.js
@@ -1,16 +1,17 @@
 let teamBoxes = document.querySelectorAll(".team-box");
-let teamBoxContainer = document.querySelector(".team-box-container");
 
 
 
 teamBoxes.forEach((box) => {
     const teamBoxOverlay = box.querySelector(".team-box-overlay");
+    const teamShareButEffect = box.querySelector(".team-share-but");
 
     if(!teamBoxOverlay) return;
 
     box.addEventListener("mouseover", () => {
         teamBoxOverlay.classList.add("team-box-overlay-effect");
         teamBoxOverlay.classList.remove("team-box-overlay-outer");
+        teamShareButEffect.classList.add(".team-share-but-effect");
     })
 
     box.addEventListener("mouseleave", (evts) => {
