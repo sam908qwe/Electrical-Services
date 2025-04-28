31b9e4da9be7cb238ea1305c0992a96cbdf65275
sam908qwe <mohammad8445sameer@gmail.com>
2025-04-28 19:32:06 +0530

    next save

--- assets/Js/ourTeam.js
+++ assets/Js/ourTeam.js
@@ -4,20 +4,22 @@ let teamBoxes = document.querySelectorAll(".team-box");
 
 teamBoxes.forEach((box) => {
     const teamBoxOverlay = box.querySelector(".team-box-overlay");
-    const teamShareButEffect = box.querySelector(".team-share-but");
+    const teamShareBut = box.querySelector(".team-share-but");
+    
 
     if(!teamBoxOverlay) return;
 
     box.addEventListener("mouseover", () => {
         teamBoxOverlay.classList.add("team-box-overlay-effect");
         teamBoxOverlay.classList.remove("team-box-overlay-outer");
-        teamShareButEffect.classList.add(".team-share-but-effect");
+        teamShareBut.classList.add("team-share-but-effect");
+        teamShareBut.classList.remove("team-share-but-outeft")
     })
 
     box.addEventListener("mouseleave", (evts) => {
             teamBoxOverlay.classList.remove("team-box-overlay-effect");
             teamBoxOverlay.classList.add("team-box-overlay-outer");
+            teamShareBut.classList.remove("team-share-but-effect");
+            teamShareBut.classList.add("team-share-but-outeft");
     } )
 })
-
-
