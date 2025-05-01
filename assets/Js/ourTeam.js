4fada2e171a5489f4c4b6e935ddd36db82b566cf
sam908qwe <mohammad8445sameer@gmail.com>
2025-05-01 16:53:47 +0530

    testinomials section add in this code

--- assets/Js/ourTeam.js
+++ assets/Js/ourTeam.js
@@ -5,6 +5,7 @@ let teamBoxes = document.querySelectorAll(".team-box");
 teamBoxes.forEach((box) => {
     const teamBoxOverlay = box.querySelector(".team-box-overlay");
     const teamShareBut = box.querySelector(".team-share-but");
+    const teamSocialHandels = box.querySelector(".team-social-handels");
     
 
     if(!teamBoxOverlay) return;
@@ -13,7 +14,10 @@ teamBoxes.forEach((box) => {
         teamBoxOverlay.classList.add("team-box-overlay-effect");
         teamBoxOverlay.classList.remove("team-box-overlay-outer");
         teamShareBut.classList.add("team-share-but-effect");
-        teamShareBut.classList.remove("team-share-but-outeft")
+        teamShareBut.classList.remove("team-share-but-outeft");
+        teamSocialHandels.style.visibility = "visible";
+        teamSocialHandels.classList.add("team-social-handels-effect");
+        teamSocialHandels.classList.remove("team-social-handels-eftOut");
     })
 
     box.addEventListener("mouseleave", (evts) => {
@@ -21,5 +25,7 @@ teamBoxes.forEach((box) => {
             teamBoxOverlay.classList.add("team-box-overlay-outer");
             teamShareBut.classList.remove("team-share-but-effect");
             teamShareBut.classList.add("team-share-but-outeft");
+            teamSocialHandels.classList.remove("team-social-handels-effect");
+            teamSocialHandels.classList.add("team-social-handels-eftOut");
     } )
 })
