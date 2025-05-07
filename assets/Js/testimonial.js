a2f63fcc737734491d971c6fa478a1f25264db3f
sam908qwe <mohammad8445sameer@gmail.com>
2025-05-07 19:47:53 +0530

    testimonial fix

---
+++ assets/Js/testimonial.js
@@ -0,0 +1,11 @@
+let testimonialContainer = document.querySelector(".testimonial-slider-container");
+let slideLeftButton = document.querySelector(".slide-left-button");
+let slideRightButton = document.querySelector(".slide-right-button");
+let idx = 0;
+let total = testimonialContainer.children.length;
+
+let mainSlides = document.querySelectorAll(".slide");
+
+function slideLeft(){
+    
+}
