e06bb27283396a42d2290c242427ce66ce8037e8
sam908qwe <mohammad8445sameer@gmail.com>
2025-05-31 18:22:34 +0530

    navbar rebuilding

--- assets/Js/navbar.js
+++ assets/Js/navbar.js
@@ -1,6 +1,11 @@
 let navInput = document.querySelector(".nav-input");
 let navButton = document.querySelector(".navbar-search-button");
+let navGoButAfter = document.querySelector(".nav-go-but-after");
+
 
 navButton.addEventListener("click", (evt) => {
     navInput.classList.add("nav-input-after");
 })
+
+
+
